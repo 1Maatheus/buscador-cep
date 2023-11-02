4ea9868225c9c6d4ceb31c90807b08b7b0e061ca
Matheus Oliveira Monteiro <matheusoliveira7227@gmail.com>
2023-11-02 01:59:55 -0300

    Commit

--- index.js
+++ index.js
@@ -2,12 +2,11 @@ const btn = document.querySelector(".btn-generator");
 const divPic = document.querySelector(".dog-container");
 const input = document.querySelector(".input");
 const name = document.querySelector(".name");
-const apiKey = "RGAPI-b12dec26-9826-46f6-ad59-aefb664e0058";
 
 const getDogPic = async () => {
-  const cep = input.value;
+  // const cep = input.value;
 
-  const apiUrl = `https://cep.awesomeapi.com.br/json/${cep}`;
+  const apiUrl = `https://api.unsplash.com/photos/random`;
   const res = await fetch(apiUrl);
   const data = await res.json();
 
